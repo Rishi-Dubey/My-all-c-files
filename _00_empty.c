a398146440dd783568c7c97f3f393905a4afe996
Rishi-Dubey <freefirestars07@gmail.com>
2024-05-26 12:40:30 +0530

    Added an simple c structure file

---
+++ _00_empty.c
@@ -0,0 +1,13 @@
+// It is just a basic c structure that every c program need 
+// I created it in order to save time by copying it.
+
+
+#include <stdio.h>
+
+
+int main()
+{
+
+    // if successful exit
+    return 0;
+}
